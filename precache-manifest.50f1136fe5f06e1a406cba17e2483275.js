7bdb1d6d6ded3c22b23057589efcc19282f51053
Raymond Tam <raymondtamtwtp@gmail.com>
2020-01-30 19:55:33 +0800

    Updates

---
+++ precache-manifest.50f1136fe5f06e1a406cba17e2483275.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4a19add3851eaba179b030964bb1068",
+    "url": "/monsters-rolodex-complete/index.html"
+  },
+  {
+    "revision": "75bde99071bc01b06296",
+    "url": "/monsters-rolodex-complete/static/css/main.d599f085.chunk.css"
+  },
+  {
+    "revision": "d499d87a993599493776",
+    "url": "/monsters-rolodex-complete/static/js/2.1e4a2dbe.chunk.js"
+  },
+  {
+    "revision": "75bde99071bc01b06296",
+    "url": "/monsters-rolodex-complete/static/js/main.d6fca86f.chunk.js"
+  },
+  {
+    "revision": "683efa6afd61321d8334",
+    "url": "/monsters-rolodex-complete/static/js/runtime~main.0e367587.js"
+  }
+]);
